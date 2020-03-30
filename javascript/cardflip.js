a3a52154f6da5689b716e55a86c8d1ae4dbcd8c9
SangBeom Park <tkdqja4164@naver.com>
2020-03-30 21:44:28 +0900

    Update cardflip.js

--- javascript/cardflip.js
+++ javascript/cardflip.js
@@ -7,6 +7,8 @@
 </head>
 <body>
     <script>
+        /*규칙 : 맨마지막을 뒤집을땐 자신이 뒤집힘
+                 카드를 뒤집으면 오른쪽카드도 뒤집힘(단, 뒤집힌 카드를 뒤집으면 오른쪽카드만 뒤집힘)*/
         var count= 0;
         var card = ["front", "front", "front", "front", "front", "front", "front", "front"];
         var isallflip = function (currentcard) { return currentcard === "front"; }
@@ -35,7 +37,6 @@
                 } 
             }
             if(card[ran] === "back"){
-                card[ran] = "front";
                 if(card[ran+1] === "front"){
                     card[ran+1] = "back";
                     return;
